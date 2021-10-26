ed734a7c7f97f8d5607b03ce510b56f14203da09
Giovanna Cunha <giovannakelli@gmail.com>
2021-10-26 00:59:55 -0300

    pwa functionality was added

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -29,8 +29,12 @@ export default class MyDocument extends Document {
 
   render() {
     return (
-      <Html>
+      <Html lang="pt-BR">
         <Head>
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="apple-touch-icon" href="/icon.png"></link>
+          <meta name="theme-color" content="#fff" />
+          <link rel="shortcut icon" href="/favicon.ico" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;700;800&display=swap" rel="stylesheet" />
         </Head>
